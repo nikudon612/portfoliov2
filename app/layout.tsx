4ec917fd7362d5b410c6eef83a3ea9fbc60574c9
Nick Bechtel <bechtel.nick@gmail.com>
2023-08-04 23:35:34 -0400

    styling for about text

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Inter, Roboto_Mono } from 'next/font/google'
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 const roboto = Roboto_Mono({
-  weight: '400',
+  weight: '400' ,
   subsets: ['latin'],
 })
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Inter } from 'next/font/google'
- 
-// If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <div className={styles.main}>
       <div className={styles.center}>
         <div className={styles.text}>
-          <div>
-            Hi, my name is <span className={styles.name}>Nick</span>.
+          <div className={styles.intro}>
+            Hi, my name is <span className={styles.name}>Nick</span><span className={styles.period}>.</span>
           </div>
           <div>I'm a software engineer based in New York City</div>
         </div>
