a3933a83afa4782c5d13c5a9bff94814f8fe0211
Nick Bechtel <bechtel.nick@gmail.com>
2023-08-04 23:22:18 -0400

    footer elements and styles

--- app/footer/footer.tsx
+++ app/footer/footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import styles from "./footer.module.scss";
 
 function footer() {
   return (
-    <div>footer</div>
-  )
+    <div className={styles.footer}>
+      <div className={styles.socials}>
+        <Link href="https://www.linkedin.com/in/nicholasbechtel/" className={styles.links}>LinkedIn</Link>
+        <Link href="https://github.com/nikudon612" className={styles.links}>GitHub</Link>
+      </div>
+    </div>
+  );
 }
 
-export default footer
+export default footer;
