bbdc7938b15bd80ac68430510be60ea4a04cbd25
Nick Bechtel <bechtel.nick@gmail.com>
2023-08-06 20:07:15 -0400

    updates to several files

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ function about() {
     <div className={styles.about}>
       <div className={styles.center}>
         <Image
-          src="/NickPolaroid-1.jpeg"
+          src="/polaroid.png"
           alt="Fujifilm 100c"
           width={750}
           height={550}
@@ -16,21 +16,26 @@ function about() {
         />
         <div className={styles.text}>
           I'm a full-stack software engineer specializing in Next.js and
-          TypeScript. Collaborating with designers to bring their visions to
-          life excites me, I thrive on building scalable, fluid, and performant
-          front-end UIs. With a love for technology and continuous learning, I
-          stay at the forefront of web development best practices. My dedication
-          to crafting exceptional user experiences drives me to deliver
-          innovative and visually appealing web applications.
+          TypeScript. Currently at{" "}
+          <Link href="https://www.perpetualny.com/" className={styles.link}>
+            Perpetaul
+          </Link>
+          . Collaborating with designers to bring their visions to life excites
+          me, I thrive on building scalable, fluid, and performant front-end
+          UIs. With a love for technology and continuous learning, I stay at the
+          forefront of web development best practices. My dedication to crafting
+          exceptional user experiences drives me to deliver innovative and
+          visually appealing web applications.
         </div>
         <div className={styles.text}>More info about my background~</div>
         <div className={styles.text}>
           Outside of work, I like to spend as much time as I can away from my
           laptop, traveling and getting outside with my camera. I most enjoy
-          shooting on one of many older film cameras such as the Canon A1 or
+          shooting with one of many older film cameras, such as my Canon A1 or
           Mamiya RZ67. Most of the 2010s I spent as a freelance photographer
-          shooting editorials, fashion, interiors and real-estate. If you feel
-          inclined to see my photo-work, you can do so on{" "}
+          shooting editorials, fashion, interiors and real-estate. I apply that
+          critical and creative mindset from my photography to my work as an
+          engineer. If you feel inclined to see my photo-work, you can do so on{" "}
           <Link
             href="https://instagram.com/wavvyboi?igshid=OGQ5ZDc2ODk2ZA=="
             target="__blank"
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './resume.module.scss'
-
+import React from "react";
+import styles from "./resume.module.scss";
+import Image from "next/image";
 function page() {
   return (
-    <div className={styles.resume}>resume</div>
-  )
+    <div className={styles.resume}>
+      <Image src="/resume.png" alt="resume" width={800} height={1100} />
+    </div>
+  );
 }
 
-export default page
+export default page;
