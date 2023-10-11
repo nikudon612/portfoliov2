563bb1319a7c812db3517996ac310e40543c728d
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2023-10-11 17:10:37 -0400

    updates to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import type { Metadata } from "next";
 import React from "react";
 import Navbar from "./navbar/page";
 import Footer from "./footer/footer";
-import { Inter, Roboto_Mono } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 const roboto = Roboto_Mono({
   weight: "400",
   subsets: ["latin"],
